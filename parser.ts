e4cf5a23e9431558012c285ce7442bc3afdf4d7f
Akito Ito <akito_ito@r.recruit.co.jp>
2018-10-11 23:01:50 +0900

    feat: parser

--- parser.ts
+++ parser.ts
@@ -1,21 +1,62 @@
-import { Project, ScriptTarget, ts, TypeAliasDeclaration, StatementedNode } from "ts-simple-ast";
+import { resolve } from "path";
+import * as ts from "typescript";
+import * as TJS from "typescript-json-schema";
 
-const fileNames = process.argv.slice(2);
-fileNames.forEach(fileName => {
-  const project = new Project({
-    compilerOptions: {
-      target: ScriptTarget.ES2015
-    }
+function main() {
+  const fileNames = process.argv.slice(2);
+  const agreedAPITypes = parse(fileNames);
+  generateSchema(fileNames, agreedAPITypes);
+}
+
+function generateSchema(fileNames, typeNames) {
+  const settings: TJS.PartialArgs = { required: true };
+  const compilerOptions: TJS.CompilerOptions = { module: ts.ModuleKind.ES2015 };
+
+  const res = f => {
+    return resolve(f);
+  };
+
+  const program = TJS.getProgramFromFiles(
+    fileNames.map(res),
+    compilerOptions,
+    __dirname
+  );
+  const generator = TJS.buildGenerator(program, settings);
+  typeNames.forEach(t => {
+    console.log(JSON.stringify(generator.getSchemaForSymbol(t)));
+  });
+}
+
+function parse(fileNames) {
+  const exportAPINames = [];
+  const program = ts.createProgram(fileNames, {
+    target: ts.ScriptTarget.ES5,
+    module: ts.ModuleKind.CommonJS
   });
 
-  project.addExistingSourceFile(fileName);
-  const sourceFile = project.getSourceFileOrThrow(fileName);
-  const types :TypeAliasDeclaration[] = sourceFile.getStatements()
-  .filter(s => {
-    return ts.isTypeAliasDeclaration(s.compilerNode);
-  })
-  .map(s => s.compilerNode);
-
-  types.forEach(t => {
+  fileNames.forEach(fileName => {
+    const sourceFile = program.getSourceFile(fileName);
+    ts.forEachChild(sourceFile, (node: ts.Node) => {
+      if (!isNodeExported(node) || !ts.isTypeAliasDeclaration(node)) {
+        return;
+      }
+      const n: any = node.type;
+      if (!n.typeName || n.typeName.escapedText !== "APIDef") {
+        return;
+      }
+      const name = node.name.escapedText;
+      exportAPINames.push(name);
+    });
   });
-});
+
+  return exportAPINames;
+}
+
+function isNodeExported(node: any): boolean {
+  return (
+    (ts.getCombinedModifierFlags(node) & ts.ModifierFlags.Export) !== 0 ||
+    (!!node.parent && node.parent.kind === ts.SyntaxKind.SourceFile)
+  );
+}
+
+main();
